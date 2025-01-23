bc588c0064cb97fc6421df9c00418d4436abc339
Lin Xinkang <47944560+risuxx@users.noreply.github.com>
2025-01-23 15:27:59 +0800

    Update setup-arl.sh

--- misc/setup-arl.sh
+++ misc/setup-arl.sh
@@ -227,3 +227,21 @@ if [ ! -f /etc/systemd/system/arl-scheduler.service ]; then
   cp misc/arl-scheduler.service /etc/systemd/system/
 fi
 
+chmod +x /opt/ARL/app/tools/*
+echo "start arl services ..."
+
+systemctl enable arl-web
+systemctl restart arl-web
+systemctl enable arl-worker
+systemctl restart arl-worker
+systemctl enable arl-worker-github
+systemctl restart arl-worker-github
+systemctl enable arl-scheduler
+systemctl restart arl-scheduler
+systemctl enable nginx
+systemctl restart nginx
+
+python3.6 tools/add_finger.py
+python3.6 tools/add_finger_ehole.py
+
+echo "install done"
